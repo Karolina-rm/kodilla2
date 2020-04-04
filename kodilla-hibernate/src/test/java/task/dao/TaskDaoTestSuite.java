6798b5e578ff90706817d572270d3f8de7a9501c
Karolina-rm <karolina.ra.kr@gmail.com>
2020-04-04 13:28:52 +0200

    Task 17.3 solution

--- kodilla-hibernate/src/test/java/task/dao/TaskDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/task/dao/TaskDaoTestSuite.java
@@ -1,5 +1,6 @@
 package task.dao;
 
+import financial.dao.TaskFinancialDetails;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -50,6 +52,23 @@ public class TaskDaoTestSuite {
 
         //CleanUp
         int id = readTasks.get(0).getId();
+        taskDao.deleteById(id);
+    }
+
+    @Test
+    public void testTaskDaoSaveWithFinancialDetails() {
+        //Given
+        Task task = new Task(DESCRIPTION, 30);
+        task.setTaskFinancialDetails(new TaskFinancialDetails(new BigDecimal(120), false));
+
+        //When
+        taskDao.save(task);
+        int id = task.getId();
+
+        //Then
+        Assert.assertNotEquals(0, id);
+
+        //CleanUp
         taskDao.deleteById(id);
     }
 }
